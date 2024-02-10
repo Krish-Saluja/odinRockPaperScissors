8ebadb1234f6fd29364040ad39f433804ba3291e
Krish Saluja <krishsaluja297@gmail.com>
2024-02-10 20:21:56 +0530

    Allows the user to input the choice and prints the result in the console.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+let pChoice = prompt("Please enter your choice!(rock, paper or scissors)");
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection){
         console.log("Draw!");
     }
-    else if (playerSelection=== "rock" && computerSelection==="paper"){
-        console.log("Computer won!");
+    else if (playerSelection=== "scissors" && computerSelection==="paper"){
+        console.log("Player won!");
     }
     else if (playerSelection=== "rock" && computerSelection==="scissors"){
         console.log("Player won!");
     }
+    else if (playerSelection=== "paper" && computerSelection==="rock"){
+        console.log("Player won!");
+    }
+    else if (playerSelection=== "rock" && computerSelection==="paper"){
+        console.log("Computer won!");
+    }
+    else if (playerSelection=== "scissors" && computerSelection==="rock"){
+        console.log("Computer won!");
+    }
+    else if (playerSelection=== "paper" && computerSelection==="scissors"){
+        console.log("Computer won!");
+    }
+    
     else {
         console.log("Please enter a Valid Choice.")
     }
@@ -15,9 +30,10 @@ function playRound(playerSelection, computerSelection) {
 
   }
   
-  const playerSelection = "rock";
+  const playerSelection = pChoice.toLowerCase();
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
+  console.log(playerSelection, computerSelection);
 
 
 function getComputerChoice() {
